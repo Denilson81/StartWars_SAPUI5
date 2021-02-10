d139e426d7d1aee5cdf8930e819b6d2d8084bff5
unknown <denilsonp@gmail.com>
2021-02-10 21:09:54 +0100

    second commit

--- starwars/webapp/controller/S0.controller.js
+++ starwars/webapp/controller/S0.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller, JSONModel) {
 		"use strict";
 
 		return Controller.extend("pina.starwars.controller.S0", {
 			onInit: function () {
-
+				
+				var characters   = this.getView().getModel("people").getData();
+				
+				var oModel = new JSONModel(characters);
+				this.getView().setModel(oModel, "peopleList");
+			
+				
+			},
+			onListItemPress: function(){
+               
 			}
 		});
 	});
